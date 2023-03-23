d3b7e3b3a44157c7d7fe46916daa68977b0395b0
João Gonçalves Maurício <joao.goncalves.mauricio@gmail.com>
2023-03-23 13:24:28 +0000

    initial basic skill commit

--- __init__.py
+++ __init__.py
@@ -5,9 +5,14 @@ class RoombaMaster(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('master.roomba.intent')
-    def handle_master_roomba(self, message):
-        self.speak_dialog('master.roomba')
+    @intent_file_handler('start.cleaning.intent')
+    def handle_start_cleaning(self, message):
+        self.speak_dialog('comply.dialog')
+
+    @intent_file_handler('stop.cleaning.intent')
+    def handle_start_cleaning(self, message):
+        self.speak('OK. Vou parar de limpar.')
+        self.speak('OK! I will stop cleaning.')
 
 
 def create_skill():
