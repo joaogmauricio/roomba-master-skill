e44ac8a1dc0c6a09807e4ebd33df908a22fa0490
João Gonçalves Maurício <joao.goncalves.mauricio@gmail.com>
2023-03-25 12:06:36 +0000

    fix ident

--- __init__.py
+++ __init__.py
@@ -3,30 +3,30 @@ import os.path
 
 class RoombaMaster(MycroftSkill):
 
-    _rooms = []
+	_rooms = []
 
-    def initialize(self):
-        self.register_entity_file('room.entity')
-        self.register_entity_file('cleaning_robot.entity')
-        filename = os.path.dirname(__file__) + '/locale/en-us/room.entity'
-        with open (filename, mode='r') as rooms_file:
-            self._rooms = rooms_file.read().splitlines()
+	def initialize(self):
+		self.register_entity_file('room.entity')
+		self.register_entity_file('cleaning_robot.entity')
+		filename = os.path.dirname(__file__) + '/locale/en-us/room.entity'
+		with open (filename, mode='r') as rooms_file:
+			self._rooms = rooms_file.read().splitlines()
 
-    @intent_handler('start.cleaning.intent')
-    def handle_start_cleaning(self, message):
-        room_utterance = message.data.get('room')
-        for room in self._rooms:
-            if room in room_utterance:
-                self.speak_dialog('comply', {'room': room})
+	@intent_handler('start.cleaning.intent')
+	def handle_start_cleaning(self, message):
+		room_utterance = message.data.get('room')
+		for room in self._rooms:
+			if room in room_utterance:
+				self.speak_dialog('comply', {'room': room})
 
-    @intent_handler('stop.cleaning.intent')
-    def handle_stop_cleaning(self, message):
-        self.speak('OK. Vou parar de limpar.')
-        self.speak('OK! I will stop cleaning.')
+	@intent_handler('stop.cleaning.intent')
+	def handle_stop_cleaning(self, message):
+		self.speak('OK. Vou parar de limpar.')
+		self.speak('OK! I will stop cleaning.')
 
-    def stop(self):
-        pass
+	def stop(self):
+		pass
 
 def create_skill():
-    return RoombaMaster()
+	return RoombaMaster()
 
